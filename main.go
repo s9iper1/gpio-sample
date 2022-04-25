24154a71ce50409ba400a83b2257df1880e8ed65
Bilal <s9iper1@gmail.com>
2022-04-25 16:43:48 +0500

    changes

--- main.go
+++ main.go
@@ -1,15 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/stianeikeland/go-rpio/v4"
 )
 
+var (
+	// Use mcu pin 10, corresponds to physical pin 19 on the pi
+	pin = rpio.Pin(21)
+)
+
 func main() {
-	pin := rpio.Pin(21)
+	if err := rpio.Open(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	// Unmap gpio memory when done
+	defer rpio.Close()
+
+	// Set pin to output mode
 	pin.Output()
 
+	// Toggle pin 20 times
 	for x := 0; x < 20; x++ {
 		pin.Toggle()
 		time.Sleep(time.Second / 5)
